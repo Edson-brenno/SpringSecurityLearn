e8a1623f2410981abd2fb5efaf9fb572729e3db5
brenno <brennorebeiro@gmail.com>
2024-11-04 22:00:16 -0300

    add: configuration for the spring security

--- SpringSecurity/src/main/java/com/spring/Controller/AuthUserController.java
+++ SpringSecurity/src/main/java/com/spring/Controller/AuthUserController.java
@@ -1,6 +1,5 @@
 package com.spring.Controller;
 
-import com.spring.Entity.AuthUser;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
