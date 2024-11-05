6a841056915785e3ee08e15d03eef511715b62c9
brenno <brennorebeiro@gmail.com>
2024-11-04 21:24:09 -0300

    add: auth user controller

---
+++ SpringSecurity/src/main/java/com/spring/Controller/AuthUserController.java
@@ -0,0 +1,43 @@
+package com.spring.Controller;
+
+import com.spring.Entity.AuthUser;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.spring.Service.AuthUserService;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import com.spring.Dtos.AuthUserDtos.*;
+
+@Controller
+@RequestMapping("/users")
+public class AuthUserController {
+
+    @Autowired
+    private AuthUserService authUserService;
+
+    @GetMapping
+    public ResponseEntity<List<AuthUserDto>> getAllUsers() {
+
+        return ResponseEntity.ok().body(authUserService.getAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<AuthUserDto> getUserById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(authUserService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AuthUserDto> createUser(HttpServletRequest request,
+                                                  @RequestBody AuthUserRegisterDto authUserDto)
+            throws URISyntaxException {
+        AuthUserDto authUser = authUserService.createUser(authUserDto);
+        String uri = request.getRequestURI() + "/" + authUser.id();
+
+        return ResponseEntity.created(new URI(uri)).body(authUser);
+    }
+}
